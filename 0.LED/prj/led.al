b322712728c4be90b930aa2d145f963be6182843
Thomas Hou <hytcqq@gmail.com>
2018-09-21 11:00:26 +0800

    1. RGB LED pin's changed to right; 2. LED add pwm control; 3. LED add breath notify;

--- 0.LED/prj/led.al
+++ 0.LED/prj/led.al
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <Project>
     <Project_Created_Time>2018-05-03 18:06:13</Project_Created_Time>
-    <TD_Version>4.2.217</TD_Version>
+    <TD_Version>4.2.511</TD_Version>
     <UCode>00000000</UCode>
     <Name>led</Name>
     <HardWare>
@@ -19,11 +19,11 @@
     <TOP_MODULE>
         <LABEL/>
         <MODULE>led</MODULE>
-        <CREATEINDEX>auto</CREATEINDEX>
+        <CREATEINDEX>user</CREATEINDEX>
     </TOP_MODULE>
     <Project_Settings>
-        <Step_Last_Change>2018-08-13 11:34:16</Step_Last_Change>
-        <Current_Step>0</Current_Step>
+        <Step_Last_Change>2018-09-21 10:53:12</Step_Last_Change>
+        <Current_Step>60</Current_Step>
         <Step_Status>true</Step_Status>
     </Project_Settings>
 </Project>
--- 0.LED/src/led.v
+++ 0.LED/src/led.v
@@ -1,46 +1,86 @@
-module led
-	(
+module led(
 		input wire CLK_IN,
 		input wire RST_N,
 		output wire [2:0]RGB_LED
-	);
+		);
 
-	parameter time1 = 24'd24_000_000;//¾§ÕñÎª24Mhz
-
-	reg [2:0]rledout;
-	reg [23:0] count;
-	reg [1:0]shift_cnt;
+reg [15:0] cnt;
+wire[7:0] dutyR, dutyG, dutyB;
+reg bln_clk;
 	
-	initial
-	begin
-		count=24'b0;
-		rledout=3'b1;
-		shift_cnt=2'b0;
+PWM PWMR(.clk(CLK_IN), .rst(RST_N), .duty(dutyR), .pwm(RGB_LED[0]));
+PWM PWMG(.clk(CLK_IN), .rst(RST_N), .duty(dutyG), .pwm(RGB_LED[1]));
+PWM PWMB(.clk(CLK_IN), .rst(RST_N), .duty(dutyB), .pwm(RGB_LED[2]));
+
+always @(posedge CLK_IN or negedge RST_N) begin
+	if(!RST_N) begin
+		cnt <= 1'b0;
+		bln_clk <= 1'b0;
 	end
-
-	always @(posedge CLK_IN)begin
-		if(RST_N==0)begin
-			count <= 24'b0;
-			rledout <= 3'b1;
-			shift_cnt <=2'b0;
-		end
-	
-		if(count == time1)
- 		begin 
- 			count<= 24'd0;
- 			
- 			if(shift_cnt==2'b10)begin
-				rledout <= 3'b1;
-				shift_cnt <=2'b0;
- 			end
- 			else begin
- 				rledout <= {rledout[1:0],1'b0};
-				shift_cnt <= shift_cnt + 1'b1;
-			end
- 		end
- 		else
- 			count <= count + 1'b1;
+	else if(cnt == 16'hFFFF) begin
+		cnt <= 0;
+		bln_clk <= ~bln_clk;
 	end
-	
-	assign RGB_LED = rledout;
+	else begin
+		cnt	<= cnt + 1'b1;
+	end
+end
+
+`define _BLN
+`ifdef _BLN
+
+BLN#(000) BLNR(bln_clk, RST_N, dutyR);
+BLN#(063) BLNG(bln_clk, RST_N, dutyG);
+BLN#(200) BLNB(bln_clk, RST_N, dutyB);
+
+`else
+
+reg[23:0] rgb;
+assign dutyR=rgb[7:0];
+assign dutyG=rgb[15:8];
+assign dutyB=rgb[23:16];
+
+always @(posedge bln_clk)
+	if(rgb == 24'hFFFFFF)
+		rgb <= 0;
+	else rgb <= rgb+1;
+
+`endif
+
+endmodule
+
+module BLN #( parameter DUTY=0) // breathing led notify
+(
+	input wire clk,	rst,
+	output reg[7:0] duty
+);
+reg r;
+always @(posedge clk or negedge rst)
+	if(!rst)
+		r <= 1'b1;
+	else if(duty == 8'hFF || duty == 8'h0)
+		r <= ~r;
+
+always @(negedge clk or negedge rst)
+	if(!rst)
+		duty <= DUTY;
+	else
+		duty = r ? duty + 1'b1 : duty - 1'b1;
+
+endmodule
+
+module PWM(
+	input wire clk, rst,
+	input wire[7:0] duty,
+	output wire pwm
+);
+reg[7:0] cnt;
+always @(posedge clk or negedge rst)
+	if(!rst)
+		cnt <= 1'b0;
+	else
+		cnt	<= cnt + 1'b1;
+
+assign pwm = cnt>=duty;
+
 endmodule
